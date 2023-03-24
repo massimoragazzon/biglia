119e44b0b370dd070ac8b857a55c58754bb0dec1
massimoragazzon <128791217+massimoragazzon@users.noreply.github.com>
2023-03-24 10:42:36 +0100

    Aggiorna main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,21 @@
+let posx = 0
+let posy = 0
+input.onButtonPressed(Button.A, function () {
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
+})
 basic.forever(function () {
-	
+    posx = Math.round(input.acceleration(Dimension.X) / 400) + 2
+    posy = Math.round(input.acceleration(Dimension.Y) / 400) + 2
+    led.plot(posx, posy)
+})
+basic.forever(function () {
+    if (0 == 0) {
+    	
+    }
 })
